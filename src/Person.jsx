1b65c4d33729be1905e81c658ec3eab0a73f1ac7
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-04-28 23:33:14 -0300

    Update Person.jsx

--- src/Person.jsx
+++ src/Person.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const Person = ({image, name, age}) => {
   return (
-    <div><h1>Person</h1></div>
+  <article className='person'>
+    <img src={image} alt={name} className='img' />
+    <div>
+      <h4>{name}</h4>
+      <p>{age} years</p>
+    </div>
+  </article>
   )
 }
