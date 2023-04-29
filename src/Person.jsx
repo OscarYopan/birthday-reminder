b5dd1dc57988b2c03571fa6fd77453431af8cff4
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-04-29 20:32:12 -0300

    Update Person.jsx

--- src/Person.jsx
+++ src/Person.jsx
@@ -9,13 +9,5 @@ export const Person = ({image, name, age}) => {
       <p>{age} years</p>
     </div>
   </article>
-
-  <article className='person'>
-  <img src={image} alt={name} className='img' />
-  <div>
-    <h4>{name}</h4>
-    <p>{age} years</p>
-  </div>
-  </article>
   )
 }
